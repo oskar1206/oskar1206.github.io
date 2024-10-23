9f1078e6e16c575ea3420824187c0d02dab0f334
oskar1206 <oararat@leonisticolamerced.edu.co>
2024-10-23 17:42:06 -0500

    initial commit

---
+++ pregunta1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+<div class="wrapper">
+<h1> ¿ QUE NOS LLAMÓ MÁS LA ATENCIÓN DEL TEXTO ?</h1>
+</div>
+</body>
+</html>
---
+++ pregunta2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+<div class="wrapper">
+<h1> ¿ QUE DIFERENCIA HAY ENTRE ACEPTARSE Y AMARSE?</h1>
+</div>
+</body>
+</html>
